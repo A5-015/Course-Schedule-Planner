e8ec81df24db617cc60e77c67f433e4eb19dd5c9
niniack <nishantaswani@gmail.com>
2019-02-21 01:16:00 +0400

    query results are now "flaskifiable" (returned in dict format)

--- backend/core/dbConnector.py
+++ backend/core/dbConnector.py
@@ -11,6 +11,25 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
+def flaskify(header = [], *args):
+
+    resultsDict = {}
+    i = 0
+    while i < len(header):
+        resultsDict[header[i]] = []
+        i += 1
+
+    i = 0
+    for arg in args:
+        j = 0
+        while j < len(arg):
+            resultsDict[header[i]].append(arg[j])
+            j += 1
+        i += 1
+
+    return resultsDict
+
+
 class Database:
 
     def returnAllMajors(self):
@@ -28,27 +47,56 @@ class Database:
     def returnMajorRequirements(self, majorID):
         courseIDQuery = "SELECT FK_course FROM appears WHERE categoryID= '" + majorID + "' AND required = 'true'"
         mycursor.execute(courseIDQuery)
-        courseIDTuple = mycursor.fetchall()
-        courseIDList = list(courseIDTuple)
+        #sql query returns a list/tuple
+        courseIDList = mycursor.fetchall()
+        #convert the courseIDs into an array
         courseIDArray = [x for xs in courseIDList for x in xs]
-        requiredCoursesTuple = []
-        print (courseIDArray)
+
+
+        #instantiate the array for course PSID
+        requiredCoursesPSID = []
         x = 0
+        #while loop goes through and finds all major requirements and places into courses list
         while x < len(courseIDArray):
             peoplesoftIDQuery = "SELECT peoplesoftID FROM course WHERE PK_course= '" + str(courseIDArray[x]) + "'"
             mycursor.execute(peoplesoftIDQuery)
-            requiredCoursesTuple.append(mycursor.fetchone())
+            requiredCoursesPSID.append(mycursor.fetchone())
             x += 1
-        requiredCoursesArray = [x for xs in requiredCoursesTuple for x in xs]
-        return requiredCoursesArray
+        #convert the required curses into a
+        requiredCoursesPSID = [x for xs in requiredCoursesPSID for x in xs]
 
+
+        #instantiate the array for course PSID
+        requiredCoursesTitle = []
+        x = 0
+        #while loop goes through and finds all major requirements and places into courses list
+        while x < len(courseIDArray):
+            titleQuery = "SELECT title FROM course WHERE PK_course= '" + str(courseIDArray[x]) + "'"
+            mycursor.execute(titleQuery)
+            requiredCoursesTitle.append(mycursor.fetchone())
+            x += 1
+        #convert the required curses into a
+        requiredCoursesTitle = [x for xs in requiredCoursesTitle for x in xs]
+
+
+        header = ['Course ID', 'Course Title']
+        reqCourses= flaskify(header, requiredCoursesPSID, requiredCoursesTitle)
+        return reqCourses
+
+    def MajorQuery(self, major):
+        query = "SELECT peoplesoftID FROM course WHERE peoplesoftID LIKE '" +  major + "%'"
+        mycursor.execute(query)
+        myresult = mycursor.fetchall()
+
+
+    #incomplete section
     def returnCourse(self, keyword):
         courseTitle = []
         keywordQuery = "SELECT title FROM course WHERE title LIKE '%" + keyword + "%'"
         mycursor.execute(keywordQuery)
         courseTitle = mycursor.fetchall()
 
-        keywordQuery = "SELECT professor from professor WHERE professor LIKE '%" + keyword + "%'"
+        keywordQuery = "SELECT FK_meeting from professor WHERE professor LIKE '%" + keyword + "%'"
         mycursor.execute(keywordQuery)
         meetingTuple = mycursor.fetchall()
 
@@ -60,14 +108,11 @@ class Database:
 
 
 
-    def MajorQuery(self, major):
-        query = "SELECT peoplesoftID FROM course WHERE peoplesoftID LIKE '" +  major + "%'"
-        mycursor.execute(query)
-        myresult = mycursor.fetchall()
+
 
 
 
 
 db = Database()
-test = db.returnCourse("fyrce")
+test = db.returnMajorRequirements("72")
 print (test)
