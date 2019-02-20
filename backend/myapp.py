e67570c49897c7476403df4b85be30a04547e4a2
Barkin Simsek <barkin.simsek@hotmail.com>
2019-02-20 22:34:25 +0400

    updated array to list conversion

--- backend/myapp.py
+++ backend/myapp.py
@@ -5,6 +5,9 @@ import mysql.connector
 app = Flask(__name__)
 application = app # web hosting requires application in passenger_wsgi
 
+##########################################################################
+########################### Table tester START ###########################
+##########################################################################
 mydb = mysql.connector.connect(
   host="localhost",
   user="bsimsekc_nishant",
@@ -14,52 +17,27 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-query = "SELECT peoplesoftID, title, writing, isCapstone FROM course WHERE peoplesoftID LIKE '" +  "ACS-UH 1011X" + "%'"
+query = "SELECT peoplesoftID, title, writing FROM course WHERE peoplesoftID LIKE '" +  "ACS-UH 2210JX" + "%'"
 mycursor.execute(query)
 myresult = mycursor.fetchall()
 
-print (myresult)
+resultsDict = {'peopleSoftId': [], 'name': [], 'professor': []}
 
-# List of lists
-cars_list = myresult
-# Output dictionary
-cars_dict = {}
-
-# Convert to dictionary
-
-cars_dict['peopleSoftId'] = [1]
-cars_dict['name'] = [1]
-cars_dict['professor'] = [1]
-cars_dict['description'] = [1]
-
-# Print to see output
-#print (cars_dict)
-
-for peopleSoftId in cars_list:
-    for i in peopleSoftId:
-        cars_dict['peopleSoftId'].append(i)
-
-for name in cars_list:
-    for i in name:
-        cars_dict['name'].append(i)
-
-for professor in cars_list:
-    for i in professor:
-        cars_dict['professor'].append(i)
-
-for description in cars_list:
-    for i in description:
-        cars_dict['description'].append(i)
-
-print (cars_dict)
+returnedResultLength = len(myresult)
+i = 0
+while i < returnedResultLength:
+    resultsDict['peopleSoftId'].append(myresult[i][0])
+    resultsDict['name'].append(myresult[i][1])
+    resultsDict['professor'].append(myresult[i][2])
+    i = i + 1
+##########################################################################
+########################### Table tester END #############################
+##########################################################################
 
 @app.route('/')
 def result():
 
-    #table = {'peopleSoftId':[0,1,2], 'name': ["name1","name2","name3"], 'professor': ["professor1","professor2","professor3"], 'description': ["description1","description2","description3"]}
-    #table = {'ACS-UH 1011X': ['Introduction to Modern Arabic Literature', 'false', 'false']}
-
-    return render_template('index.html', table = cars_dict)
+    return render_template('index.html', table = resultsDict)
 
 @app.route("/test")
 def test():
